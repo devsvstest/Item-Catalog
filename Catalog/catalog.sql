8a07d36b8c780adcb7566621109b92ef1f8c8244
Shruthi V S <ram2016svs@gmail.com>
2019-04-13 03:18:26 +0530

    Deleted redundant catalog.sql and images folder.

--- Catalog/catalog.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE catalog(id SERIAL PRIMARY KEY, 
-                    title VARCHAR(80) NOT NULL,
-                    category VARCHAR(80), 
-                    description VARCHAR(250),
-                    TIMESTAMP,
-                      );
-
